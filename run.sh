87d2d6c05d50c609d447fc35a282fca0dfded6d9
plopz0r <alain@schaufelwat.ch>
2015-06-08 10:33:38 +0200

    added twittbook stuff

--- run.sh
+++ run.sh
@@ -8,4 +8,6 @@ find /app/ -name db -exec rm {}/.htdb.db \;
 find /app/ -name db -exec chown -R www-data:www-data {} \;
 /usr/sbin/sshd
 /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE twittbook;"
+mysql -uroot twittbook < /var/www/html/twittbook/install.sql
 exec supervisord -n
